6551f53cd47f2bf0ef73439e91cc25bd6dee41ab
Umidjon <hackerumif7@gmail.com>
2022-05-22 15:41:35 +0500

    OfferSerializer is now ModelSerializer

--- backend/offers/serializers.py
+++ backend/offers/serializers.py
@@ -1,19 +1,8 @@
-from rest_framework import serializers
-from users.models import RegularUser, Company
+from rest_framework.serializers import ModelSerializer
 from .models import Offer
 
 
-class OfferSerializer(serializers.Serializer):
-    id = serializers.IntegerField(label='ID', read_only=True)
-    date = serializers.DateTimeField(read_only=True)
-    user = serializers.PrimaryKeyRelatedField(queryset=RegularUser.objects.all())
-    company = serializers.PrimaryKeyRelatedField(queryset=Company.objects.all())
-
-    def create(self, validated_data):
-        return Offer.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.user = validated_data.get('user', instance.user)
-        instance.company = validated_data.get('company', instance.company)
-        instance.save()
-        return instance
+class OfferSerializer(ModelSerializer):
+    class Meta:
+        model = Offer
+        fields = "__all__"
